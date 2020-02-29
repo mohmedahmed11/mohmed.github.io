10f06449e2edd15a191a136befa429af1f72b571
mohmedahmed11 <albrer112@gmail.com>
2020-02-29 13:47:34 +0200

    new add

---
+++ compress app/ViewController copy.swift
@@ -0,0 +1,328 @@
+//
+//  ViewController.swift
+//  compress app
+//
+//  Created by MacBook Pro on 27/06/1441 AH.
+//  Copyright © 1441 Pita Studio. All rights reserved.
+//
+
+import UIKit
+import Compression
+
+class ViewController: UIViewController {
+    
+    let sourceData = """
+    Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.Lorem ipsum dolor sit amet consectetur adipiscing elit mi
+    nibh ornare proin blandit diam ridiculus, faucibus mus
+    dui eu vehicula nam donec dictumst sed vivamus bibendum
+    aliquet efficitur. Felis imperdiet sodales dictum morbi
+    vivamus augue dis duis aliquet velit ullamcorper porttitor,
+    lobortis dapibus hac purus aliquam natoque iaculis blandit
+    montes nunc pretium.
+    """.data(using: .utf8)!
+    
+    let pageSize = 128
+    
+    var compressedData = Data()
+    var decompressedData = Data()
+    
+    var str1 = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        compressData()
+        
+        str1 = String(bytes: compressedData.base64EncodedData(), encoding: .ascii)!
+        
+        print("\(str1)")
+        decompressData()
+        let str = String(data: decompressedData, encoding: .ascii)!
+        print("\(  str)")
+        // Do any additional setup after loading the view.
+    }
+    
+    func compressData() {
+        do {
+            let outputFilter = try OutputFilter(.compress,
+                                                 using: .lzfse) { (data: Data?) -> Void in
+                if let data = data {
+                    self.compressedData.append(data)
+                }
+            }
+                
+            var index = 0
+            let bufferSize = sourceData.count
+            
+            while true {
+                let rangeLength = min(pageSize, bufferSize - index)
+                
+                let subdata = sourceData.subdata(in: index ..< index + rangeLength)
+                index += rangeLength
+
+                try outputFilter.write(subdata)
+
+                if (rangeLength == 0) {
+                    break
+                }
+            }
+        } catch {
+            fatalError("Error occurred during encoding: \(error.localizedDescription).")
+        }
+    }
+    
+    
+    
+    func decompressData() {
+        do {
+            var index = 0
+            let compressiveString = Data(base64Encoded: str1)!
+            
+            let bufferSize = compressiveString.count
+            
+            
+            
+            print(compressiveString)
+            
+            let inputFilter = try InputFilter(.decompress,
+                                              using: .lzfse) { (length: Int) -> Data? in
+                let rangeLength = min(length, bufferSize - index)
+                let subdata = compressiveString.subdata(in: index ..< index + rangeLength)
+                index += rangeLength
+                
+                return subdata
+            }
+            
+            while let page = try inputFilter.readData(ofLength: pageSize) {
+                decompressedData.append(page)
+            }
+        
+        } catch {
+            fatalError("Error occurred during decoding: \(error.localizedDescription).")
+        }
+    
+    }
+
+}
